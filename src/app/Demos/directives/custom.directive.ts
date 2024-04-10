f79e8a98f72bcb6828fa76efc8bc7c0f88da1de1
afbstorm <aurelien.freyling@bstorm.be>
2024-04-10 11:05:08 +0200

    Ajouts de commentaires sur les démos

--- src/app/Demos/directives/custom.directive.ts
+++ src/app/Demos/directives/custom.directive.ts
@@ -6,6 +6,9 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 })
 export class CustomDirective {
 
+  // ElementRef : C'est un wrapper autour d'un élément natif dans le View. Cela peut être utilisé pour manipuler les propriétés de l'élément DOM.
+  // Renderer2: C'est une classe abstraite qui fournit des méthodes pour manipuler l'UI. Ceci est plus sûr que ElementRef car il donne plus de possibilités de rendre votre application compatible avec le serveur.
+
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2
--- src/app/Demos/inout/child/child.component.ts
+++ src/app/Demos/inout/child/child.component.ts
@@ -8,12 +8,16 @@ import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '
   styleUrl: './child.component.scss'
 })
 export class ChildComponent implements OnChanges{
+
+  // Le décorateur @Input() défnini les propriétés que le composant parent peut passer au composant enfant.
   @Input() info!: string;
   @Input() num1!: number;
   @Input() num2!: number;
 
+  // Le décorateur @Output() permet de créer un EventEmitter qui émet un événement au composant parent lorsqu'un calcul a lieu.
   @Output() calculToutCa: EventEmitter<number> = new EventEmitter<number>()
 
+  // ngOnChanges fait partie du cycle de vie Angular qui est appelée lorsque les valeurs liées en entrée changent. Si num1 ou num2 change, elle déclenche le calcul.
   ngOnChanges(changes:SimpleChanges) {
     if ('num1' in changes || 'num2' in changes) {
       this.calculate();
